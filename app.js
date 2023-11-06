a547b8697eec7bcffa096fa95ce13094120775a4
Muhammad Asif1163 <bibbasheikh11334@gmail.com>
2023-11-07 00:32:20 +0500

    2 files create

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+var a = 2;
+var b = 1;
+
+var c = --a - --b + ++b + b--;
+
+alert(3)
+
+
+
+var d = 5;
+var g = 8;
+
+var e = d++ + ++g;
+
+alert(e = 14)
+
+
+
+
+
+var d = 5;
+var g = 8;
+var h = 13;
+
+var e = d++ + ++g + h++ ;
+
+alert(e = 27)
+
+
+
+var d = 5;
+var g = 8;
+var h = 13;
+
+var e = ++d - g++ + ++h ;
+
+alert(12)
+
+
+
+var d = 5;
+var g = 8;
+var h = 13;
+
+var e = ++d - g++ - h++ ;
+
+alert(-15)
